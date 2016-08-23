7f6f76745758c5af64bafcf07b7533fad5f75d4b
cibin <ikonland@gmail.com>
2016-08-23 23:53:42 +0530

    second

--- port_files.py
+++ port_files.py
@@ -7,20 +7,20 @@ action = None
 file_list = None
 
 try:
-    options, remainder = getopt.getopt(sys.argv[1:], 'o:a', ['output=', 
-                                                         'action',
-                                                         'version=',
-                                                         ])
-except getopt.GetoptError:                                     
+    options, remainder = getopt.getopt( sys.argv[1:], 'o:a:h', ['output=', 'action=', 'help']
+                                       )
+except getopt.GetoptError:
+    print('error')
     sys.exit(2)                      
-
+print (options)
 for opt, arg in options:
+    print(opt, arg)
     if opt in ('-o', '--output'):
         file_list = arg
         print(file_list)
     elif opt in ('-a', '--action'):
         action = arg
-        
+        print(arg)
 
 if file_list is None or action is None:
     print('missing arguments')
@@ -31,15 +31,17 @@ import os
 
 all_files=open(file_list).read().splitlines()
 parent = os.path.dirname(os.path.abspath(__file__))
-print ('adsasdfs')
 
-if action=='to_repo':
+print(action)
+if action=='repo_to_folder':
     for f in all_files:
         shutil.copyfile(os.path.join(parent,'ported_files', os.path.basename(f) ),f )
-else if action=='repo_to_folder':
+        print('restored from repo to ' + str(f))
+elif action =='to_repo':
     for f in all_files:
         shutil.copyfile(f,os.path.join(parent,'ported_files', os.path.basename(f)) )
-        
+        print('copied to repo from ' + str(f))
+
         
         
         
