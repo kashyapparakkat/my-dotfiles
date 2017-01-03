187fd709c053f2b15a79f66c2607dbaadc026b48
cibin <ikonland@gmail.com>
2017-01-03 20:56:05 +0530

    added more files

--- port_files.py
+++ port_files.py
@@ -1,9 +1,20 @@
 # cbn
-# script_file.py to_repo/from_repo files_to_port-<installation-detail>
+# script_file.py -a into_this_repo/from_this_repo_to_folder -o files_to_port-<installation-detail>
 
+from distutils.dir_util import copy_tree
 import getopt
 import sys
-
+import errno
+ 
+def copy_files_and_directories(src, dest):
+    try:
+        shutil.copytree(src, dest)
+    except OSError as e:
+        # If the error was caused because the source wasn't a directory
+        if e.errno == errno.ENOTDIR:
+            shutil.copy(src, dest)
+        else:
+            print('Directory not copied. Error: %s' % e)
 action = None
 file_list = None
 
@@ -34,16 +45,23 @@ all_files=open(file_list).read().splitlines()
 parent = os.path.dirname(os.path.abspath(__file__))
 
 print(action)
-if action=='repo_to_folder':
-    print('Danger: overwriting')
+if action=='from_this_repo_to_folder':
+    print('Danger: overwriting: yes?')
     a=str(input())
-    if (a=='y'): 
+    if (a=='yes'): 
         for f in all_files:
             shutil.copyfile(os.path.join(parent,'ported_files', os.path.basename(f) ),f )
             print('restored from repo to ' + str(f))
-elif action =='to_repo':
+elif action =='into_this_repo':
     for f in all_files:
-        shutil.copyfile(f,os.path.join(parent,'ported_files', os.path.basename(f)) )
+
+        # copy subdirectory example
+        # fromDirectory = "/a/b/c"
+        # toDirectory = "/x/y/z"
+        if os.path.isdir(f):
+            copy_tree(f,os.path.join(parent,'ported_files', os.path.basename(f)) )
+        else:
+            shutil.copyfile(f,os.path.join(parent,'ported_files', os.path.basename(f)) )
         print('copied to repo from ' + str(f))
 
         
---
+++ ported_files/cibin/cron_jobs.sh
@@ -0,0 +1,6 @@
+source $HOME/set_defaults.sh
+source $HOME/misc_lib.sh
+echo $Universal_home
+myindexemacs
+myindex
+
